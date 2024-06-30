480dd136f958646cd7a45c03c83557c8f9076969
Alexander Fletcher <alex@alexanderfletcher.dev>
2024-06-30 12:58:00 +1000

    update favicon

--- src/pages/updates/index.tsx
+++ src/pages/updates/index.tsx
@@ -20,9 +20,14 @@ export default function BlogPage() {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
-            Clocktower Updates
-          </h1>
+          <div className="flex">
+            <Link href={'/'}>
+            <Image alt="Clocktower.fyi logo" height={40} width={40} src={'/favicon.svg'} className="md:w-[48px]" />
+          </Link>
+              <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
+                Clocktower Updates
+              </h1>
+            </div>
           <p className="text-xl text-muted-foreground">
             An unofficial collection of updates on the social deduction game{" "}
             <Link
