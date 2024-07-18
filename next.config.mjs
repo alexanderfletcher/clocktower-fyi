aa2b08040b4d16066118363e733f9ff6c053a6d4
Alexander Fletcher <16837325+alexanderfletcher@users.noreply.github.com>
2024-07-18 22:03:17 +1000

    add speculated links

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/speculated",
+        destination:
+          "https://www.bloodstar.xyz/p/piglix/Full_Unreleased_Characters/almanac.html",
+        permanent: false,
+      },
+      {
+        source: "/speculated/editions",
+        destination:
+          "https://www.bloodstar.xyz/p/piglix/Unreleased_Editions/almanac.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
